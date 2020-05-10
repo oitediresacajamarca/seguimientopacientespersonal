e820a0960debb3c97344bcb54d7c4de7f7683d9d
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-05-10 18:51:26 -0500

    se inicia el trabajo de los formatos

--- src/app/componetes/tabla-notificacion/tabla-notificacion.component.ts
+++ src/app/componetes/tabla-notificacion/tabla-notificacion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ɵConsole, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ɵConsole, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { SolicitudesService } from 'src/app/servicios/solicitudes.service';
 import { MorbilidadesService } from 'src/app/servicios/morbilidades.service';
 import { MenuItem } from 'primeng/api/menuitem';
@@ -10,115 +10,125 @@ import { Router } from '@angular/router';
   templateUrl: './tabla-notificacion.component.html',
   styleUrls: ['./tabla-notificacion.component.css']
 })
-export class TablaNotificacionComponent implements OnInit {
-      pacientes:any[];
-      pacientesSelected:any[];
-      cols:any[]
-      @Output()  displayNotificacion:EventEmitter<any>= new EventEmitter();
-      cod_ambito:string;
-      selectedNoti: any;
-      selectCars: any[];
-      elementosmenu: MenuItem[];
+export class TablaNotificacionComponent implements OnInit,AfterViewInit {
+  pacientes: any[];
+  pacientesSelected: any[];
+  cols: any[]
+  @Output() displayNotificacion: EventEmitter<any> = new EventEmitter();
+  cod_ambito: string;
+  selectedNoti: any;
+  selectCars: any[];
+  elementosmenu: MenuItem[];
 
 
 
-  constructor(private sol:SolicitudesService,private morb:MorbilidadesService,private router:Router) { }
+  constructor(private sol: SolicitudesService, private morb: MorbilidadesService, private router: Router) { }
+  ngAfterViewInit(): void {
+  this.cargarNotificaciones()
+  }
 
   ngOnInit() {
-          this.elementosmenu= [
-            { label: 'Registrar Atencion', icon: 'pi-user-edit', command: (event) =>{   this.router.navigate(['/admin/atencion/'+this.selectedNoti.NRO_DOCUMENTO+'/'+this.selectedNoti.ID_PACIENTE+'/'+this.selectedNoti.ID_SOLICITUD]);
-            console.log(this.selectedNoti);this.displayNotificacion.emit('cerrar')}  },
-            { label: 'Esperar', icon: 'pi pi-times', command: (event) => {}}
-        ];
-  
-        let SESION:any=JSON.parse(localStorage.getItem('datos'));
+    this.cargarNotificaciones()
+  }
+
+  cargarNotificaciones() {
+    this.elementosmenu = [
+      {
+        label: 'Registrar Atencion', icon: 'pi-user-edit', command: (event) => {
+          this.router.navigate(['/admin/atencion/' + this.selectedNoti.NRO_DOCUMENTO + '/' + this.selectedNoti.ID_PACIENTE + '/' + this.selectedNoti.ID_SOLICITUD]);
+          console.log(this.selectedNoti); this.displayNotificacion.emit('cerrar')
+        }
+      },
+      { label: 'Esperar', icon: 'pi pi-times', command: (event) => { } }
+    ];
+
+    let SESION: any = JSON.parse(localStorage.getItem('datos'));
 
 
-        let distrito:string=SESION.ID_DISTRITO;
-        this.cod_ambito=SESION.COD_AMBITO_GEOGRAFICO; 
-          
-
-        this.sol.devolverColumnasSolicitudes().subscribe((res)=>{
-            this.cols=res.respuesta;          
-        })
-  
-      let maximo_cie=0;
-          this.sol.devolverDatosSolicitudes(this.cod_ambito).subscribe((datos)=>{
-              let solicitudes=datos.respuesta;
-              let solicitudesedit=[]
-
-              var max_col_sol=0;
-
-              solicitudes.forEach(solicitud => {         
-             
-                let solicitudedit=solicitud;
-                this.morb.devolverMorbildadPaciente(solicitud.ID_PACIENTE).subscribe((dato)=>{
-                     max_col_sol=0;
-                    let morbilidades=dato.respuesta;
-                    morbilidades.forEach((morvilidad) => {
-
-                      max_col_sol=max_col_sol+1;
-                      solicitudedit['ID_MEDICAMENTO_'+max_col_sol]=morvilidad.ID_MEDICAMENTO;
-                      solicitudedit['ID_CIE_'+max_col_sol]=morvilidad.ID_CIE;
-                      
-                        if(max_col_sol>maximo_cie){                           
-                          
-                            maximo_cie=max_col_sol;
-                            let col={'header':'ID_MEDICAMENTO_'+maximo_cie,'field':'ID_MEDICAMENTO_'+maximo_cie};
-                            this.cols.push(col);
-                            col=  {'header':'ID_CIE_'+maximo_cie,'field':'ID_CIE_'+maximo_cie};
-                            this.cols.push(col);
-                            console.log('LEGANO'+max_col_sol);            
-
-                        }
+    let distrito: string = SESION.ID_DISTRITO;
+    this.cod_ambito = SESION.COD_AMBITO_GEOGRAFICO;
 
 
+    this.sol.devolverColumnasSolicitudes().subscribe((res) => {
+      this.cols = res.respuesta;
+    })
+
+    let maximo_cie = 0;
+    this.sol.devolverDatosSolicitudes(this.cod_ambito).subscribe((datos) => {
+      let solicitudes = datos.respuesta;
+      let solicitudesedit = []
+
+      var max_col_sol = 0;
+
+      solicitudes.forEach(solicitud => {
+
+        let solicitudedit = solicitud;
+        this.morb.devolverMorbildadPaciente(solicitud.ID_PACIENTE).subscribe((dato) => {
+          max_col_sol = 0;
+          let morbilidades = dato.respuesta;
+          morbilidades.forEach((morvilidad) => {
+
+            max_col_sol = max_col_sol + 1;
+            solicitudedit['ID_MEDICAMENTO_' + max_col_sol] = morvilidad.ID_MEDICAMENTO;
+            solicitudedit['ID_CIE_' + max_col_sol] = morvilidad.ID_CIE;
+
+            if (max_col_sol > maximo_cie) {
+
+              maximo_cie = max_col_sol;
+              let col = { 'header': 'ID_MEDICAMENTO_' + maximo_cie, 'field': 'ID_MEDICAMENTO_' + maximo_cie };
+              this.cols.push(col);
+              col = { 'header': 'ID_CIE_' + maximo_cie, 'field': 'ID_CIE_' + maximo_cie };
+              this.cols.push(col);
+              console.log('LEGANO' + max_col_sol);
+
+            }
 
 
 
 
-                      
-                    });
 
-
-                    
-                });
-                
-               
-               
-                
-              });
-
-
-            
-          
-
-             this.pacientes=solicitudes;
 
 
           });
 
-     }
 
-     
-exportExcel() {
-  import("xlsx").then(xlsx => {
+
+        });
+
+
+
+
+      });
+
+
+
+
+
+      this.pacientes = solicitudes;
+
+
+    });
+
+  }
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
       const worksheet = xlsx.utils.json_to_sheet(this.pacientes);
       const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
       const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
       this.saveAsExcelFile(excelBuffer, "primengTable");
-  });
-}
+    });
+  }
 
-saveAsExcelFile(buffer: any, fileName: string): void {
-  import("file-saver").then(FileSaver => {
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    import("file-saver").then(FileSaver => {
       let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
       let EXCEL_EXTENSION = '.xlsx';
       const data: Blob = new Blob([buffer], {
-          type: EXCEL_TYPE
+        type: EXCEL_TYPE
       });
       FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-  });
-}
+    });
+  }
 
 }
