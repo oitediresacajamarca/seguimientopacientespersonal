bd47b47b2682351ae5381b811c043a2a1493e04a
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-05-27 13:48:05 -0500

    se corrigio los nombres que permita las enies

--- src/app/componentes/public/public.component.ts
+++ src/app/componentes/public/public.component.ts
@@ -48,7 +48,7 @@ export class PublicComponent implements OnInit {
   telefcon2: string;
   correo: string;
   es: any;
-  letrasyespacio: RegExp = /[a-zA-Z ñÑ]/;
+  letrasyespacio: RegExp = /[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+/;
   vermensajeconfirmacion: boolean = false;
   @ViewChild('datosgenerales', { static: false })
   datosgenerales: NgForm;
